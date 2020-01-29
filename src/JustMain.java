0556b8ba93083a32212f18b2076a2d144339de1d
Gregory <purkaqm@mail.ru>
2020-01-29 20:56:19 +0300

    Initial commit

---
+++ src/JustMain.java
@@ -0,0 +1,21 @@
+
+public class JustMain {
+    String field ="";
+    Runnable runner;
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public Runnable getRunner() {
+        return runner;
+    }
+
+    public void setRunner(Runnable runner) {
+        this.runner = runner;
+    }
+}
---
+++ src/Runner.java
@@ -0,0 +1,8 @@
+
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println("Start is here");
+
+        new JustMain().setField("Main");
+    }
+}
